f1f3ab085beb7cccc7bb21e5f0cc0e532fe2db54
gogajanan <gajanan.shinde@godeskless.com>
2022-08-12 18:43:33 +0530

    certain changes in python

---
+++ index.cjs
@@ -0,0 +1,15 @@
+const { PythonShell } = require("python-shell");
+let pyshell = new PythonShell("main.py");
+pyshell.send("hello");
+pyshell.on("message", function (message) {
+  console.log(message);
+});
+setTimeout(() => {
+  pyshell.send("Another Hello");
+}, 3000);
+const end = () => {
+  pyshell.end(function (err, code, signal) {
+    if (err) throw err;
+    console.log("finished");
+  });
+};
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+import sys
+    
+def log():  
+    print('hello from python')
+while True:
+    inp = input(" ")
+    if inp=="hello":
+        log()
