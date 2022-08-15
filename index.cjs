c0a871abc4e75fa149e64a91af9853b7587c99eb
gogajanan <gajanan.shinde@godeskless.com>
2022-08-15 15:48:01 +0530

    export modules

--- index.cjs
+++ index.cjs
@@ -1,16 +1,21 @@
 const { PythonShell } = require("python-shell");
 let pyshell = new PythonShell("main.py");
-pyshell.send("hello");
-pyshell.on("message", function (message) {
-  console.log(message);
-});
-setTimeout(() => {
-  pyshell.send("Another Hello");
-}, 3000);
-const end = () => {
-  pyshell.end(function (err, code, signal) {
-    if (err) throw err;
-    console.log("finished");
+
+function abc() {
+  pyshell.send("hello");
+  pyshell.on("message", function (message) {
+    console.log(message);
   });
 };
-module.exports.pyshell =pyshell;
+module.exports =  abc();
+// setTimeout(() => {
+//   pyshell.send("Another Hello");
+// }, 3000);
+// const end = () => {
+//   pyshell.end(function (err, code, signal) {
+//     if (err) throw err;
+//     console.log("finished");
+//   });
+// };
+// module.exports.pyshell = pyshell;
+// abc();
